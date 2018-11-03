6ef7e972ac88702239c31681d0d74c0baf93ff8b
trotzinger <tim.rotzinger@gmail.com>
2018-11-03 16:42:45 -0600

    just starting

---
+++ wordgame.py
@@ -0,0 +1,13 @@
+'''
+Game where you try to make all the words you can given some letters
+
+author Tim Rotzinger 03/11/2018
+
+'''
+import cmd
+
+WORDS_FILE_PATH = 
+
+if __name__ == "__main__":
+    print ("Done.")
+	
