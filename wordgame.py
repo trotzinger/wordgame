9b3f0e47192d9a080f639e15b173bde71950c9a7
trotzinger <tim.rotzinger@gmail.com>
2018-11-03 17:11:02 -0600

    added the word list and got really sleepy so didnt actually do anything

--- wordgame.py
+++ wordgame.py
@@ -6,8 +6,24 @@ author Tim Rotzinger 03/11/2018
 '''
 import cmd
 
-WORDS_FILE_PATH = 
+WORDS_FILE_PATH = "C:\wordgame\words.txt"
 
+class Words(object):
+    def __init__(self):
+        self.words = None
+        with open(WORDS_FILE_PATH, 'r') as wordsfile:
+            self.words = wordsfile.read().strip()
+    def words_from_letters(self, letters):
+        '''
+        input:  letters, a list of letters to build words from
+        output: a set of words that can be built from the letters
+        
+        Given some letters, return a set where the values are the 
+        words that can be made from the letters (no repeats)
+        '''
+        
 if __name__ == "__main__":
+    words = Words()
+    print (words.words)
     print ("Done.")
 	
