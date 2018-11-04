acd0c5087c068e2bc11a2dcd4ca11c3a8e7a315a
trotzinger <tim.rotzinger@gmail.com>
2018-11-03 18:50:35 -0600

    trying to get word filter working

--- wordgame.py
+++ wordgame.py
@@ -12,7 +12,8 @@ class Words(object):
     def __init__(self):
         self.words = None
         with open(WORDS_FILE_PATH, 'r') as wordsfile:
-            self.words = wordsfile.read().strip()
+            self.words = wordsfile.read().strip().split()
+            
     def words_from_letters(self, letters):
         '''
         input:  letters, a list of letters to build words from
@@ -21,9 +22,23 @@ class Words(object):
         Given some letters, return a set where the values are the 
         words that can be made from the letters (no repeats)
         '''
+        words_from_letters = []
+        print (letters)
+        for word in self.words:
+            tmp_word = list(word)
+            print (tmp_word)
+            flag = True
+            for letter in letters:
+                if letter not in tmp_word:
+                    flag = False
+            if flag:
+                words_from_letters.append(word)
+        return words_from_letters
         
 if __name__ == "__main__":
     words = Words()
-    print (words.words)
+    #print (words.words[:10])
+    l = ['a', 't', 'r', 'd', 'u']
+    print (words.words_from_letters(l))
     print ("Done.")
 	
